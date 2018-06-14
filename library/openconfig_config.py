0f10262798ab7fa0ac1bef2ba91e9544527aeb6e
Deepak Agrawal <deepacks@gmail.com>
2018-06-14 13:20:43 +0000

    working write for all data models

--- library/openconfig_config.py
+++ library/openconfig_config.py
@@ -59,6 +59,8 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network.common.netconf import NetconfConnection
 from ansible.module_utils.network.netconf.netconf import dispatch
 from ansible.module_utils._text import to_bytes
+from ansible.utils.path import unfrackpath, makedirs_safe
+
 try:
     from ncclient.xml_ import to_xml
     HAS_NCCLIENT = True
@@ -85,6 +87,8 @@ except ImportError:
 _EDIT_OPS = frozenset(['merge', 'create', 'replace', 'delete'])
 
 BASE_1_0 = "{urn:ietf:params:xml:ns:netconf:base:1.0}"
+
+YANG_SEARCH_PATH = "~/.ansible/tmp/yang"
 
 class Config(object):
 
@@ -127,7 +131,6 @@ class Config(object):
         found = False
         # Search for schema in schema supported by device
         for index, schema_list in enumerate(self._schema_cache):
-            q (index, to_bytes(schema_list["identifier"], errors='surrogate_or_strict'), schema_key)
             if to_bytes(schema_key) == to_bytes(schema_list["identifier"], errors='surrogate_or_strict'):
                 self._schema["identifier"] = schema_key
                 self._schema["namespace"] = self._schema_cache[index]["namespace"]
@@ -145,7 +148,7 @@ class Config(object):
            res_json = jxmlease.parse(res)
            data_json = res_json["rpc-reply"]["data"]
            q(data_json)
-        
+
         return found, data_json
 
 class OpenConfig(object):
@@ -153,6 +156,10 @@ class OpenConfig(object):
         self._module = module
         self._data = data
         self._schema = schema
+        # main yang data model for the defined schema
+        self._data_model = None
+        # list of all dependant data model from the device
+        self._dep_data_model = []
 
     def create_config(self):
         config = Config(self._module)
@@ -160,10 +167,63 @@ class OpenConfig(object):
            found, data_model = config.get_schema(schema)
            if found:
              self._data_model = data_model
+             self.get_depend_data_models()
+             self.copy_data_models_in_searchpath(YANG_SEARCH_PATH)
            else:
              #TODO raise exception
              self._data_model = None
 
+    def get_depend_data_models(self):
+        importre = re.compile(r'import (.+) {')
+        dep_data_model_list = importre.findall(self._data_model)
+
+        config = Config(self._module)
+        for data_model_key in dep_data_model_list:
+            key = to_bytes(data_model_key, errors='surrogate_or_strict')
+            found, data_model = config.get_schema(data_model_key)
+            if found:
+               self._dep_data_model.append({key: data_model})
+               self.copy_data_model_in_searchpath(YANG_SEARCH_PATH,
+                                                   (key + '.yang'),
+                                                        data_model)
+
+    def copy_data_model_in_searchpath(self, searchpath, filename, data_model):
+        searchpathdir = unfrackpath(searchpath)
+        makedirs_safe(searchpathdir)
+        final_file = os.path.join(searchpathdir, filename)
+        with open(final_file, 'w') as f:
+            f.write(data_model)
+
+    def copy_data_models_in_searchpath(self, searchpath):
+        for schema in self._schema:
+            self.copy_data_model_in_searchpath(searchpath,
+                      (schema + '.yang'), self._data_model)
+
+class PyangLib(object):
+    def __init__(self):
+        self._path = None
+
+    def find_path_module(self, modulename):
+        paths = os.environ['PATH'].split(os.pathsep)
+        for dirname in paths:
+            fullpath = os.path.join(dirname, filename)
+            if os.path.isfile(fullpath):
+            	return fullpath
+   
+    def run(self, data_model_file, searchpath, output):
+        import imp
+        
+        pyang_exe_path = self.find_path_module('pyang')
+        pyang_exec = imp.load_source('pyang', pyang_exe_path)
+ 
+        sys.argv = [pyang_exec_path, '-p', searchpath, data_model_file,
+                              '-f', 'sample-xml-skeleton', -o , output]
+        try:
+	    pyang_exec.run()
+        except SystemExit:
+            pass
+        return
+    
 
 def main():
     """main entry point for module execution
